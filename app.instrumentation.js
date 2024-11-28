8b1dc6508e1bd72231d366dbd97d6b6c2b492edd
jpbarto <jason.p.barto@gmail.com>
2024-11-28 22:14:11 +0000

    added logging, traces, and metrics to the driver

--- app.instrumentation.js
+++ app.instrumentation.js
@@ -1,8 +1,9 @@
-const opentelemetry = require('@opentelemetry/sdk-node')
+const { NodeSDK, tracing, logs } = require('@opentelemetry/sdk-node')
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node')
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc')
 const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc')
 const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics')
+const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino')
 const grpc = require('@grpc/grpc-js')
 
 const exporterConfig = {
@@ -10,12 +11,22 @@ const exporterConfig = {
   credentials: grpc.ChannelCredentials.createInsecure()
 }
 
-const sdk = new opentelemetry.NodeSDK({
+const sdk = new NodeSDK({
+  spanProcessor: new tracing.SimpleSpanProcessor(new tracing.ConsoleSpanExporter()),
+  logRecordProcessor: new logs.SimpleLogRecordProcessor(new logs.ConsoleLogRecordExporter()),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(exporterConfig)
   }),
   traceExporter: new OTLPTraceExporter(exporterConfig),
-  instrumentations: [getNodeAutoInstrumentations()],
-  serviceName: 'test-api',
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new PinoInstrumentation({
+      logHook: (span, record) => {
+        record['resource.service.name'] = span.instrumentationLibrary.name
+        record['resource.service.version'] = span.instrumentationLibrary.version
+      }
+    }),
+  ],
+  serviceName: 'simple-express-app',
 })
 sdk.start()
--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express")
+const pino = require('pino');
+
+const logger = pino ();
 const app = express()
 
 app.get("/", (req, res) => {
+  logger.info ('Processing index request');
+
   setTimeout(() => {
     res.send("Hello World")
+    logger.info ('Responded to caller');
   }, 1000);
 })
 
 app.listen(8080, () => {
-  console.log(`Listening for requests on http://localhost:8080`)
+  logger.info(`Listening for requests on http://localhost:8080`)
 })
